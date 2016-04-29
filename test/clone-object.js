bd37631279505459e745181779e8a577d768752c
Charles <charlesg.github@gmail.com>
2016-04-29 12:31:54 +0100

    Clone object testing
    Using Object.assign

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,7 +1,8 @@
 describe('clone object', function () {
   it('should clone an object', function () {
-    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
+    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']};
+
+    var obj = Object.assign({}, expected);
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
